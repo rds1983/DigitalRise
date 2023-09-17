86937fce9cb88689389424cea75d10d22a532b8e
Roman Shapiro <eleroman@mail.ru>
2023-09-18 06:38:02 +0700

    Some DigitalRune.Tests fixes

--- Source/DigitalRune.Tests/ObjectHelperTest.cs
+++ Source/DigitalRune.Tests/ObjectHelperTest.cs
@@ -160,11 +160,7 @@ namespace DigitalRune.Tests
       Assert.IsTrue(Numeric.AreEqual(va.X, 1.01f));
       Assert.IsTrue(Numeric.AreEqual(va.Y, 2.02f));
 
-#if WINDOWS_PHONE
-      var vb = (VectorB)ObjectHelper.Parse(typeof(VectorB), "(1.01; 2.02)");
-#else
-      var vb = (VectorB)ObjectHelper.Parse(typeof(VectorB), "(1,01; 2,02)");      // Use this if culture in the test is de.
-#endif
+      var vb = (VectorB)ObjectHelper.Parse(typeof(VectorB), "(1.01; 2.02)");      // Use this if culture in the test is de.
       Assert.IsTrue(Numeric.AreEqual(vb.X, 1.01f));
       Assert.IsTrue(Numeric.AreEqual(vb.Y, 2.02f));
 
