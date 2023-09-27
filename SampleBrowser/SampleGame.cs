a2f7d8b4cbbc6ddc663b716f1e04d27c5e649684
Roman Shapiro <eleroman@mail.ru>
2023-09-27 07:57:12 +0700

    MonoGame.DirectX version somehow works

--- SampleBrowser/SampleGame.cs
+++ SampleBrowser/SampleGame.cs
@@ -262,7 +262,7 @@ namespace Samples
 			// SampleFramework
 			// The SampleFramework automatically discovers all samples using reflection, provides 
 			// controls for switching samples and starts the initial sample.
-			var initialSample = typeof(CameraMotionBlurSample);
+			var initialSample = typeof(OceanSample);
 			var assetManager = AssetManager.CreateFileAssetManager(Path.Combine(Utility.ExecutingAssemblyDirectory, "../../../../Assets"));
       DefaultAssets.DefaultFont = assetManager.LoadFontSystem("Fonts/DroidSans.ttf").GetFont(32);
 			DefaultAssets.DefaultTheme = assetManager.LoadTheme("UI Themes/BlendBlue/Theme.xml", GraphicsDevice);
