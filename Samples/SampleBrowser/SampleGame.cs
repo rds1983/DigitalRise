cfec7d66b3600911900715b8f7851497fca2d1ef
Roman Shapiro <eleroman@mail.ru>
2023-09-30 08:05:01 +0700

    Refactoring

--- Samples/SampleBrowser/SampleGame.cs
+++ Samples/SampleBrowser/SampleGame.cs
@@ -263,8 +263,8 @@ namespace Samples
 			// The SampleFramework automatically discovers all samples using reflection, provides 
 			// controls for switching samples and starts the initial sample.
 			var initialSample = typeof(OceanSample);
-			var assetManager = AssetManager.CreateFileAssetManager(Path.Combine(Utility.ExecutingAssemblyDirectory, "../../../../Assets"));
-      DefaultAssets.DefaultFont = assetManager.LoadFontSystem("Fonts/DroidSans.ttf").GetFont(32);
+			var assetManager = AssetManager.CreateFileAssetManager(Path.Combine(Utility.ExecutingAssemblyDirectory, "../../../../../Assets"));
+      DefaultAssets.DefaultFont = assetManager.LoadFontSystem("Fonts/DroidSans.ttf").GetFont(16);
 			DefaultAssets.DefaultTheme = assetManager.LoadTheme("UI Themes/BlendBlue/Theme.xml", GraphicsDevice);
 
 			_services.Register(typeof(AssetManager), null, assetManager);
